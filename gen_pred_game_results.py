a9a0091949e3fee841857dd0ce65e42596a49e06
Wang Haotian <tomriddlewang@gmail.com>
2024-04-17 10:14:11 +0800

    update evaluation

--- gen_pred_game_results.py
+++ gen_pred_game_results.py
@@ -64,7 +64,8 @@ if __name__ == "__main__":
     currentDir = os.getcwd()
     outputPath = currentDir + "/output"
     outputCSVPath = os.path.join(outputPath, "game_winner_summary.csv")
-    writeLineToFile(outputCSVPath, "game_id,team_type,prob_home_low,prob_home_high,prob_away_low,prob_away_high,winner,home_expected_score,away_expected_score", 'w')
+    # writeLineToFile(outputCSVPath, "game_id,team_type,prob_home_low,prob_home_high,prob_away_low,prob_away_high,winner,home_expected_score,away_expected_score", 'w')
+    writeLineToFile(outputCSVPath, "game_id,team_type,prob_home_low,prob_home_high,prob_away_low,prob_away_high,home_expected_score,away_expected_score", 'w')
     for root, dirs, files in os.walk(outputPath):
         for file in files:
             print(f"Processing file: {file}")
@@ -76,9 +77,11 @@ if __name__ == "__main__":
                 outputPathAway = os.path.join(root, f"game_prob_{game_id}_away.txt")
                 # Predict the game result
                 winner, probHome, probAway, home_expected_score, away_expected_score = extractGameResultSoftmax(outputPathHome, outputPathAway)
-                if winner:
-                    print(f"Game ID: {game_id}, Team type: {team_type}, Winner: {winner}")
-                    writeLineToFile(outputCSVPath, f"{game_id},{team_type},{probHome[0]},{probHome[1]},{probAway[0]},{probAway[1]},{winner},{home_expected_score},{away_expected_score}", 'a')
-                else:
-                    print(f"Game ID: {game_id}, Team type: {team_type}, No winner")
-                    writeLineToFile(outputCSVPath, f"{game_id},{team_type},{probHome[0]},{probHome[1]},{probAway[0]},{probAway[1]},{home_expected_score},{away_expected_score}", 'a')
+                # if winner:
+                #     print(f"Game ID: {game_id}, Team type: {team_type}, home_expected_score: {home_expected_score}, away_expected_score: {away_expected_score}")
+                #     writeLineToFile(outputCSVPath, f"{game_id},{team_type},{probHome[0]},{probHome[1]},{probAway[0]},{probAway[1]},{winner},{home_expected_score},{away_expected_score}", 'a')
+                # else:
+                #     print(f"Game ID: {game_id}, Team type: {team_type}, No winner")
+                #     writeLineToFile(outputCSVPath, f"{game_id},{team_type},{probHome[0]},{probHome[1]},{probAway[0]},{probAway[1]},{home_expected_score},{away_expected_score}", 'a')
+                print(f"Game ID: {game_id}, Team type: {team_type}, home_expected_score: {home_expected_score}, away_expected_score: {away_expected_score}")
+                writeLineToFile(outputCSVPath, f"{game_id},{team_type},{probHome[0]},{probHome[1]},{probAway[0]},{probAway[1]},{home_expected_score},{away_expected_score}", 'a')
