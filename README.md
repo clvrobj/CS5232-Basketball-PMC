96121fb8628fae5ce7a47c273bda09aca985faee
Chi Zhang <clvrobj@gmail.com>
2024-04-25 13:40:00 +0800

    Refactor gen_pred_game_results.py

--- README.md
+++ README.md
@@ -1,6 +1,16 @@
 # CS5232-Basketball-PMC
 
 ## Steps
+```
+python py_scripts/get_game_stats.py
+
+python py_scripts/run_game.py
+
+python py_scripts/gen_pred_game_results.py
+```
+
+
+## Steps (deprecated)
 * Run the `get-game-stats.ipynb` to generate one game data and PCSP files
 * Run the `run_game.py` to predict the game probability result
 * To predict the result of the games, run the `gen_pred_game_results.py`
--- py_scripts/run_game.py
+++ py_scripts/run_game.py
@@ -1,3 +1,7 @@
+"""
+This script runs the PAT Console for all the games in the pcsp_files directory.
+Output raw PAT verification results are saved in the output directory. E.g. output/game_prob_1_home.txt
+"""
 import subprocess
 import os
 import re
@@ -54,30 +58,5 @@ def run_all_games():
     return
 
 if __name__ == "__main__":
-    #### Set PAT Console, input and output path
-    # currentDir = os.getcwd()
-    # PATConsoleFilePath = "C:\\Program Files (x86)\\PAT343\\PAT3.Console.exe" # PAT3.Console.exe doesn't work in PAT 3.5.1. Use 3.4.3 or lower.
-    # pcspFilePath = currentDir + "\\game.pcsp"
-    # outputPath = currentDir + "\\output\\game_prob"
-
-    # # Create output directory if not exists
-    # if not os.path.exists(os.path.dirname(outputPath)):
-    #     try:
-    #         os.makedirs(os.path.dirname(outputPath))
-    #     except OSError as exc:
-    #         pass
-    
-    # #### Find the game PCSP files
-    # for root, dirs, files in os.walk(currentDir+"\\pcsp_files"):
-    #     for file in files:
-    #         if re.match(r"game_[0-9]+_[a-zA-Z]+\.pcsp", file):
-    #             pcspFilePath = os.path.join(root, file)
-    #             # Extract the game ID and team type from file name
-    #             game_id, team_type, _ = os.path.basename(pcspFilePath).split(".")[0].split("_")[1:]
-    #             print("Predicting game ID: ", game_id, " Team type: ", team_type)
-    #             outputPathTmp = f"{outputPath}_{game_id}_{team_type}.txt"
-    #             executePATConsole(PATConsoleFilePath, pcspFilePath, outputPathTmp)
-    #             print(f"done for {game_id}_{team_type}")
-
     run_all_games()
         
